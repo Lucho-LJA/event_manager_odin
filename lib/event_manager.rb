5c6f933cffd419d6b8948d48eaa7261c6c3fbe97
Lucho-LJA <ljallauca.a@gmail.com>
2022-04-18 16:38:41 -0500

    Create personal letters with library erb

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -1,20 +1,23 @@
 require 'csv'
 require 'google/apis/civicinfo_v2'
+require 'erb'
 
 
 
 def legislators_by_zipcode(zip)
     civic_info = Google::Apis::CivicinfoV2::CivicInfoService.new
     civic_info.key = 'AIzaSyClRzDqDh5MsXwnCWi0kOiiBivP6JsSyBw'
+    
     begin
-      legislators = civic_info.representative_info_by_address(
+      civic_info.representative_info_by_address(
         address: zip,
         levels: 'country',
         roles: ['legislatorUpperBody', 'legislatorLowerBody']
-      )
-      legislators = legislators.officials
-      legislator_names = legislators.map(&:name)
-      legislator_names.join(", ")
+      ).officials
+      #legislators = legislators.officials
+      #legislator_names = legislators.map(&:name)
+      #legislator_names.join(", ")
+
     rescue
       'You can find your representatives by visiting www.commoncause.org/take-action/find-elected-officials'
     end
@@ -27,7 +30,8 @@ end
 puts 'Event Manager Initialized!'
 
 path_file = "event_attendees.csv"
-path_template_letter = 'template_letter.html'
+path_template_letter = 'template_letter.erb'
+
 
 if File.exist?(path_file) and File.exist?(path_template_letter)
     contest = CSV.open(
@@ -36,13 +40,13 @@ if File.exist?(path_file) and File.exist?(path_template_letter)
         header_converters: :symbol
     )
     template_letter = File.read(path_template_letter)
+    erb_template = ERB.new template_letter
 
     contest.each do |row|
         name = row[:first_name]
         zipcode = clean_zipcode(row[:zipcode])
         legislators = legislators_by_zipcode(zipcode)
-        personal_letter = template_letter.gsub('FIRST_NAME', name)
-        personal_letter.gsub!('LEGISLATORS',legislators)
-        puts personal_letter
+        form_letter = erb_template.result(binding)
+        puts form_letter
     end
 end
