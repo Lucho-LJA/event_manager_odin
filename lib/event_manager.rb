4e0ad000aaf94d61f25f45e94e1e44b026c30269
Lucho-LJA <ljallauca.a@gmail.com>
2022-04-18 11:02:30 -0500

    Add row_index var to skip the first line

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -3,8 +3,10 @@ puts 'Event Manager Initialized!'
 path_file = "event_attendees.csv"
 if File.exist?(path_file)
     lines = File.readlines(path_file)
+    row_index = 0
     lines.each do |line|
-        next if line == " ,RegDate,first_Name,last_Name,Email_Address,HomePhone,Street,City,State,Zipcode\n"
+        row_index += 1
+        next if row_index == 1
         colums = line.split(",")
         name = colums[2]
         puts name
