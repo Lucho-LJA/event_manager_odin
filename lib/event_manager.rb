5016b1acfa6f2b8ca9b3440ba56045507b2b802a
Lucho-LJA <ljallauca.a@gmail.com>
2022-04-18 15:44:14 -0500

    Add legislators_by_zipcode method and with install google_apis_civic_v2

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -1,5 +1,23 @@
 require 'csv'
+require 'google/apis/civicinfo_v2'
 
+
+def legislators_by_zipcode(zip)
+    civic_info = Google::Apis::CivicinfoV2::CivicInfoService.new
+    civic_info.key = 'AIzaSyClRzDqDh5MsXwnCWi0kOiiBivP6JsSyBw'
+    begin
+      legislators = civic_info.representative_info_by_address(
+        address: zip,
+        levels: 'country',
+        roles: ['legislatorUpperBody', 'legislatorLowerBody']
+      )
+      legislators = legislators.officials
+      legislator_names = legislators.map(&:name)
+      legislator_names.join(", ")
+    rescue
+      'You can find your representatives by visiting www.commoncause.org/take-action/find-elected-officials'
+    end
+end
 
 def clean_zipcode(zipcode)
     zipcode.to_s.rjust(5, '0')[0..4]
@@ -17,6 +35,7 @@ if File.exist?(path_file)
     contest.each do |row|
         name = row[:first_name]
         zipcode = clean_zipcode(row[:zipcode])
-        puts "#{name}  #{zipcode}"
+        legislators = legislators_by_zipcode(zipcode)
+        puts "#{name}  #{zipcode} #{legislators}"
     end
 end
