bf7d72c34aad7fb87d71e9b53d1c618d9cac45bc
Lucho-LJA <ljallauca.a@gmail.com>
2022-04-18 11:17:32 -0500

    Add csv library to read file and show the first_name colum

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -1,12 +1,11 @@
+require 'csv'
 puts 'Event Manager Initialized!'
 
 path_file = "event_attendees.csv"
 if File.exist?(path_file)
-    lines = File.readlines(path_file)
-    lines.each_with_index do |line, index|
-        next if index == 0
-        colums = line.split(",")
-        name = colums[2]
+    contest = CSV.open(path_file, headers: true)
+    contest.each do |row|
+        name = row[2]
         puts name
     end
 end
